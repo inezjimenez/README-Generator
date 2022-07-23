cc095e6b2df62d0122b40f96a4db9dc0be6f0325
Inez  Mercedes Jimenez <ijim9182@yahoo.com>
2022-07-22 21:40:56 -0500

    Finalized

---
+++ MainCode/index.js
@@ -0,0 +1,123 @@
+// TODO: Include packages needed for this application
+
+const fs = required('fs');
+const inquirer = required('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate : titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title.');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license does your project use?',
+        choices: ['None', 'Apache 2.0', 'MIT', 'GPL v3.0'],
+        validate: licenseInput = () => {
+            if (licenseInput) {
+                return true;
+            } else {
+                console.log('Please select one of the four options.')
+                return false;
+                }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What steps are needed to install your project?',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please provide installation steps.');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the use of your project?',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please provide a use for your project.');
+                return false;
+            }
+        }
+    },
+    
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What guidlines must others follow in order to contribute?',
+        validate: contributionsInput => {
+            if (contributionsInput) {
+                return true;
+            } else {
+                console.log('Please enter contribution guidelines.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How do you test this project?',
+        validate: testingInput => {
+            if (testingInput) {
+                return true;
+            } else {
+                console.log('Please explain how to test this project.');
+                return false;
+            }
+        }
+    },
+];
+
+// TODO: Create a function to write README file
+const writeToFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./generatedREADME.md', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok:true
+            })
+        })
+    })
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(question)
+    .then(function(data) {
+        console.log(data);
+    var fileContent = generateMarkdown(data);
+    writeToFile(fileContent)
+    });
+}
+
+// Function call to initialize app
+init();
+
+//export
+module.exports = questions;
